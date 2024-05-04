3c19e91744c6d7a5caf8fcc932faadc2d1e586ff
Alperen Akyol <a-akyol@hotmail.com>
2024-05-04 12:21:06 +0300

    unix hello world

--- main.s
+++ main.s
@@ -1,18 +1,29 @@
-.section .data
-hello_msg:
-    .ascii "Hello, World!\n"
-    hello_msg_len = . - hello_msg
+# ----------------------------------------------------------------------------------------
+# Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+# To assemble and run:
+#
+#     gcc -c hello.s && ld hello.o && ./a.out
+#
+# or
+#
+#     gcc -nostdlib hello.s && ./a.out
+# ----------------------------------------------------------------------------------------
 
-.section .text
-.globl _start
+        .global _start
+
+        .text
 _start:
-    mov $1, %rax             # syscall number for sys_write
-    mov $1, %rdi             # file descriptor 1 (stdout)
-    lea hello_msg(%rip), %rsi   # address of the message
-    mov hello_msg_len(%rip), %rdx # message length
-    syscall
+        # write(1, message, 13)
+        mov     $1, %rax                # system call 1 is write
+        mov     $1, %rdi                # file handle 1 is stdout
+        mov     $message, %rsi          # address of string to output
+        mov     $13, %rdx               # number of bytes
+        syscall                         # invoke operating system to do the write
 
-    mov $60, %rax            # syscall number for sys_exit
-    xor %rdi, %rdi           # exit code 0
-    syscall
-    
+        # exit(0)
+        mov     $60, %rax               # system call 60 is exit
+        xor     %rdi, %rdi              # we want return code 0
+        syscall                         # invoke operating system to exit
+message:
+        .ascii  "Hello, world\n"
+        
