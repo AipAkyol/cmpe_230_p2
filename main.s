518c6ee12e83f2790bb432f49b6c86b75146ed96
Alperen Akyol <a-akyol@hotmail.com>
2024-05-05 02:37:40 +0300

    Update main.s

--- main.s
+++ main.s
@@ -1,34 +1,56 @@
-.section .data
-    binary_str: .ascii "00000000\n"   # String to store binary representation, initialize with 8 zeros and a newline
-.section .text
-.global _start
+.data
+    value: .word 0
 
-_start:
-    mov $0, %ecx          # Counter for the loop
-    mov $8, %edx          # Number of bits to process (assuming 8-bit integer)
-    
-bit_loop:
-    shl $1, %eax          # Shift the integer value in %eax left by 1 bit
-    jnc zero_bit          # Jump if carry flag is not set (bit shifted out was 0)
-    movb $'1', binary_str(%ecx)  # Set the corresponding character in binary_str to '1'
-    jmp continue_loop
-    
-zero_bit:
-    movb $'0', binary_str(%ecx)  # Set the corresponding character in binary_str to '0'
-    
-continue_loop:
-    inc %ecx              # Increment the loop counter
-    dec %edx              # Decrement the bits counter
-    jnz bit_loop          # Jump back to bit_loop if there are more bits to process
-    
-    # Print the binary representation
-    mov $4, %eax          # syscall number for sys_write
-    mov $1, %edi          # file descriptor 1 (stdout)
-    mov $binary_str, %rsi # address of binary_str
-    mov $9, %edx          # length of binary_str (including newline)
+.text
+.global main
+main:
+    mov $value, %edi        # Load the address of value into edi
+    call read_int           # Call the function to read an integer
+    movl (%edi), %eax       # Load the value from memory into eax
+
+    # Limit value to 12 bits
+    andl $0xFFF, %eax
+
+    # Print binary representation
+    movl $12, %ecx          # Set loop counter to 12
+loop_start:
+    movl $1, %edx           # Prepare mask
+    shll $11, %edx          # Shift mask to the leftmost bit
+    andl %edx, %eax         # Apply mask
+    testl %eax, %eax        # Test if the bit is set
+    jnz bit_set             # Jump if bit is set
+    movl $'0', %ebx         # Move '0' to ebx
+    jmp print_char          # Jump to print character
+
+bit_set:
+    movl $'1', %ebx         # Move '1' to ebx
+
+print_char:
+    movl $1, %eax           # syscall number for sys_write
+    movl $1, %edi           # file descriptor 1 (stdout)
+    movq %rbx, %rsi         # character to print
+    movq $1, %rdx           # number of bytes to print (1 byte)
     syscall
 
-    # Exit the program
-    mov $60, %eax         # syscall number for sys_exit
-    xor %edi, %edi        # exit code 0
+    shll $1, %eax           # Shift right mask for next bit
+    loop loop_start         # Repeat for remaining bits
+
+    movl $10, %ebx          # Move newline character to ebx
+    movl $1, %eax           # syscall number for sys_write
+    movl $1, %edi           # file descriptor 1 (stdout)
+    movl %ebx, %esi         # character to print
+    movl $1, %edx           # number of bytes to print (1 byte)
     syscall
+
+    movl $60, %eax          # syscall number for sys_exit
+    xorl %edi, %edi         # return code 0
+    syscall
+
+read_int:
+    # Prepare arguments for syscall sys_read
+    movq $0, %rax           # syscall number for sys_read
+    movq $0, %rdi           # file descriptor 0 (stdin)
+    movq %edi, %rsi         # buffer to read into
+    movq $4, %rdx           # number of bytes to read (4 bytes)
+    syscall
+    ret
