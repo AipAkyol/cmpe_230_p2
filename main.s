c6da6c5be951095d52817d4e658bc52e430e36db
Alperen Akyol <a-akyol@hotmail.com>
2024-05-05 03:04:02 +0300

    Update main.s

--- main.s
+++ main.s
@@ -1,56 +1,83 @@
-.data
-    value: .word 0
+.section .bss
+input_buffer: .space 256            # Allocate 256 bytes for input buffer
 
-.text
-.global main
-main:
-    mov $value, %edi        # Load the address of value into edi
-    call read_int           # Call the function to read an integer
-    movl (%edi), %eax       # Load the value from memory into eax
+.section .data
 
-    # Limit value to 12 bits
-    andl $0xFFF, %eax
+.newline.str:
+    .ascii "\n"
 
-    # Print binary representation
-    movl $12, %ecx          # Set loop counter to 12
-loop_start:
-    movl $1, %edx           # Prepare mask
-    shll $11, %edx          # Shift mask to the leftmost bit
-    andl %edx, %eax         # Apply mask
-    testl %eax, %eax        # Test if the bit is set
-    jnz bit_set             # Jump if bit is set
-    movl $'0', %ebx         # Move '0' to ebx
-    jmp print_char          # Jump to print character
+.whitespace.str:
+    .ascii " "
 
-bit_set:
-    movl $'1', %ebx         # Move '1' to ebx
+.section .text
+.global _start
 
-print_char:
-    movl $1, %eax           # syscall number for sys_write
-    movl $1, %edi           # file descriptor 1 (stdout)
-    movq %rbx, %rsi         # character to print
-    movq $1, %rdx           # number of bytes to print (1 byte)
+
+# r9: input
+# r13: single char of the input
+# r10: current number
+# r14: operation reg1
+# r15: operation reg2
+_start:
+    # Read input from standard input
+    mov $0, %eax                    # syscall number for sys_read
+    mov $0, %edi                    # file descriptor 0 (stdin)
+    lea input_buffer(%rip), %rsi    # pointer to the input buffer
+    mov $256, %edx                  # maximum number of bytes to read
+    syscall                         # perform the syscall
+
+    mov %rsi, %r9
+    mov $0, %r10
+
+    loop_start:
+        movzbq (%r9), %r13  # Move one byte from memory at address stored in r9 to r13
+        
+        cmp $'\n', %r13                # Compare the character with newline ('\n')
+        je loop_end                # If it's null character, end the loop
+        
+        cmp $' ', %r13                # Compare the character with whitespace
+        jnz addition
+           
+        # implement stack and computations here
+        # TODO: opertaiondan sonra 2 ekle ki stacke operation atmasın
+        # TODO: stacke pushladıktan sonra r10 u sıfıra esitle
+        push %r10
+        mov $0, %r10
+
+        add $1, %r9
+        jmp loop_start             # Continue looping
+
+        addition:
+        cmp $'+', %r13                # Compare the character with whitespace
+        jnz number_constructor
+
+        pop %r15
+        pop %r14
+
+        add %r15, %r14
+        push %r14
+
+        mov $0, %r14
+        mov $0, %r15
+
+        # todo: implement prints
+
+        add $2, %r9
+        jmp loop_start             # Continue looping
+
+        number_constructor:
+        mov $10, %rax    # for decimal significancy
+        mul %r10
+        mov %rax, %r10 # multiplacation result
+        sub $48, %r13   # ascii to number
+        add %r13, %r10 # append the new decimal to the rest
+
+        
+        add $1, %r9
+        jmp loop_start             # Continue looping
+    loop_end:
+
+    # Exit the program
+    mov $60, %eax               # syscall number for sys_exit
+    xor %edi, %edi              # exit code 0
     syscall
-
-    shll $1, %eax           # Shift right mask for next bit
-    loop loop_start         # Repeat for remaining bits
-
-    movl $10, %ebx          # Move newline character to ebx
-    movl $1, %eax           # syscall number for sys_write
-    movl $1, %edi           # file descriptor 1 (stdout)
-    movl %ebx, %esi         # character to print
-    movl $1, %edx           # number of bytes to print (1 byte)
-    syscall
-
-    movl $60, %eax          # syscall number for sys_exit
-    xorl %edi, %edi         # return code 0
-    syscall
-
-read_int:
-    # Prepare arguments for syscall sys_read
-    movq $0, %rax           # syscall number for sys_read
-    movq $0, %rdi           # file descriptor 0 (stdin)
-    movq %edi, %rsi         # buffer to read into
-    movq $4, %rdx           # number of bytes to read (4 bytes)
-    syscall
-    ret
